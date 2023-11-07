cd7f840a90c8940e022d4b588ca129d9b3763631
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2023-11-08 00:37:07 +0800

    Update build-electron-binutils.sh

--- build-electron-binutils.sh
+++ build-electron-binutils.sh
@@ -2,31 +2,33 @@
 
 #清理
 rm -rf binutils-* build-binutils
+mkdir -p /root/Toolchain
 #设置binutils版本和安装路径
 export BV=binutils-2.31.1
 export IN=/root/Toolchain/electron-$BV
 #设置binutils版本可从https://mirrors.aliyun.com/gnu/binutils/寻找
 
 
-# Build binutils
-msg "正在构建binutils中"
+#构建binutils 
+echo "正在下载binutils中"
 wget https://mirrors.aliyun.com/gnu/binutils/$BV.tar.xz
 tar -xf $BV.tar.xz
 rm *.xz
+
+echo "正在构建64位binutils中"
 mkdir build-binutils && cd build-binutils 
-
-
 env CFLAGS=-O2 CXXFLAGS=-O2 ../$BV/configure --prefix=$IN --target=aarch64-linux-gnu --disable-compressed-debug-sections --disable-gdb --disable-werror --enable-deterministic-archives --enable-new-dtags --enable-plugins --enable-threads --quiet --with-system-zlib --disable-multilib --disable-nls --with-gnu-as --with-gnu-ld --enable-gold --enable-ld=default --with-pkgversion="Electron Binutils"
-
 make -j8 && make install
 cd ..
 rm -rf build-binutils 
 
+echo "正在构建32位binutils中"
 mkdir build-binutils && cd build-binutils
-
 env CFLAGS=-O2 CXXFLAGS=-O2 ../$BV/configure --prefix=$IN --disable-compressed-debug-sections --disable-gdb --disable-werror --enable-deterministic-archives --enable-new-dtags --enable-plugins --enable-threads --quiet --with-system-zlib --disable-multilib --disable-nls --with-gnu-as --with-gnu-ld --program-prefix=arm-linux-gnueabi- --target=arm-linux-gnueabi --with-pkgversion="Electron Binutils"
 make -j8 && make install
 
+echo "正在打包压缩binutils中"
 cd /root/Toolchain
 tar -I pixz -cf electron-$BV.tar.xz electron-$BV
+rm -rf electron-$BV
 
